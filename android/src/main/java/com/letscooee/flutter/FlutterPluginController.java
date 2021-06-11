d59a1dabfdb2a97272e9a9d48be23f5186cfc842
Ashish <ashish@wizpanda.com>
2021-06-11 22:18:27 +0530

    refactor: AppController renamed to FlutterPluginController COOEE-196

--- android/src/main/java/com/letscooee/flutter/FlutterPluginController.java
+++ android/src/main/java/com/letscooee/flutter/FlutterPluginController.java
@@ -1,14 +1,16 @@
 package com.letscooee.flutter;
 
+import com.letscooee.init.AppController;
+
 /**
  * Manage and bind {@link ActivityLifecycleCallbacks} and
- * by extending {@link com.letscooee.init.AppController} gives control to
+ * by extending {@link AppController} gives control to
  * Native Android SDK.
  *
  * @author Ashish Gaikwad on 11/06/21
  * @version 0.0.24
  */
-public class AppController extends com.letscooee.init.AppController {
+public class FlutterPluginController extends AppController {
 
     @Override
     public void onCreate() {
