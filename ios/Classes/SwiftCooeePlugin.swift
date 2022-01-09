3a07c0ecb0d80bcced2060a63a159dbf4b212e5b
Ashish Gaikwad <ashish@letscooee.com>
2022-01-09 18:42:23 +0530

    refactor: Update plugin iOS code wrt new iOS SDK COOEE-539

--- ios/Classes/SwiftCooeePlugin.swift
+++ ios/Classes/SwiftCooeePlugin.swift
@@ -2,12 +2,13 @@ import UIKit
 import CooeeSDK
 
 public class SwiftCooeePlugin: NSObject, FlutterPlugin {
-    var sdkInstance = Cooee.shared
+    var sdkInstance = CooeeSDK.getInstance()
     
     static public func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "cooee_plugin", binaryMessenger: registrar.messenger())
         let instance = SwiftCooeePlugin()
-        var sdk = Cooee.shared
+        AppController.configure()
+        var sdk = CooeeSDK.getInstance()
         registrar.addMethodCallDelegate(instance, channel: channel)
         
         channel.setMethodCallHandler({
@@ -16,45 +17,57 @@ public class SwiftCooeePlugin: NSObject, FlutterPlugin {
             //instance.handle(call, result:result)
 
                       if call.method == "getUDID"{
-                          let UDID = Cooee.shared.fetchUDID() ?? ""
+                          let UDID = sdk.getUserID() ?? ""
                           result(UDID)
                       }
 
                       if call.method == "sendEvent"{
-                          if let eventParams = call.arguments as? [String: Any]{
-                              if let eventName = eventParams["eventName"] as? String, let eventProperties = eventParams["eventProperties"] as? [String: Any]{
-                                sdk.sendEvent(withName: eventName, properties: eventProperties)
-                                  result("Event sent")
-                              }
+                          guard let eventParams = call.arguments as? [String: Any] else{
+                            return
                           }
+
+                          guard let eventName = eventParams["eventName"] as? String else{
+                            return
+                          }
+
+                          guard let eventProperties = eventParams["eventProperties"] as? [String:Any] else{
+                            return
+                          }
+
+                          do{
+                            try sdk.sendEvent(eventName: eventName,eventProperties: eventProperties);
+                            result("Event sent")
+                          }catch{}
                       }
 
                       if call.method == "updateUserProperties"{
-                      print("updateUserProperties")
-                          if let userProperties = call.arguments as? [String: Any]{
-                            sdk.updateProfile(withProperties: userProperties, andData: nil)
-                              result("User Properties Updated")
-                          }
+                        guard let userProperties = call.arguments as? [String: Any] else{
+                            return
+                        }
+
+                        sdk.updateUserProperties(userProperties: userProperties);
                       }
 
                       if call.method == "updateUserData"{
-                      print("updateUserData")
-                          if let userData = call.arguments as? [String: Any]{
-                            if let data=userData["userData"] as? [String:Any]{
-                                print("**** \(data)")
-                                sdk.updateProfile(withProperties: nil, andData: data)
-                                  result("User Data Updated")
-                            }
-                            
+                          guard let userData = call.arguments as? [String: Any] else{
+                            return
                           }
+
+                          sdk.updateUserData(userData: userData)
+                          result("User Data Updated")
                       }
 
                       if call.method == "setCurrentScreen"{
-                      print("setCurrentScreen")
-                          if let screenName = call.arguments as? String{
-                            sdk.screenName = screenName
-                              result("Screen name set")
+                          guard let arguments = call.arguments as? [String: Any] else{
+                            return
                           }
+
+                          guard let screenName = arguments["screenName"] as? String else{
+                            return
+                          }
+
+                          sdk.setCurrentScreen(screenName: screenName)
+                          result("Screen name set")
                       }
            })
     }
@@ -63,38 +76,57 @@ public class SwiftCooeePlugin: NSObject, FlutterPlugin {
    
 
         if call.method == "getUDID"{
-            let UDID = Cooee.shared.fetchUDID() ?? ""
+            let UDID = sdkInstance.getUserID() ?? ""
             result(UDID)
         }
-        
+
         if call.method == "sendEvent"{
-            if let eventParams = call.arguments as? [String: Any]{
-                if let eventName = eventParams["eventName"] as? String, let eventProperties = eventParams["eventProperties"] as? [String: Any]{
-                        sdkInstance.sendEvent(withName: eventName, properties: eventProperties)
-                        result("Event sent")
-                }
+            guard let eventParams = call.arguments as? [String: Any] else{
+                return
             }
+
+            guard let eventName = eventParams["eventName"] as? String else{
+                return
+            }
+
+            guard let eventProperties = eventParams["eventProperties"] as? [String:Any] else{
+                return
+            }
+
+            do{
+                try self.sdkInstance.sendEvent(eventName: eventName,eventProperties: eventProperties);
+                result("Event sent")
+            }catch{}
         }
-        
+
         if call.method == "updateUserProperties"{
-            if let userProperties = call.arguments as? [String: Any]{
-                sdkInstance.updateProfile(withProperties: userProperties, andData: nil)
-                result("User Properties Updated")
+            guard let userProperties = call.arguments as? [String: Any] else{
+                return
             }
+
+            self.sdkInstance.updateUserProperties(userProperties: userProperties);
         }
-        
+
         if call.method == "updateUserData"{
-            if let userData = call.arguments as? [String: Any]{
-                sdkInstance.updateProfile(withProperties: nil, andData: userData)
-                result("User Data Updated")
+            guard let userData = call.arguments as? [String: Any] else{
+                return
             }
+
+            self.sdkInstance.updateUserData(userData: userData)
+            result("User Data Updated")
         }
-        
+
         if call.method == "setCurrentScreen"{
-            if let screenName = call.arguments as? String{
-                sdkInstance.screenName = screenName
-                result("Screen name set")
+            guard let arguments = call.arguments as? [String: Any] else{
+                return
             }
+
+            guard let screenName = arguments["screenName"] as? String else{
+                return
+            }
+
+            self.sdkInstance.setCurrentScreen(screenName: screenName)
+            result("Screen name set")
         }
     }
 }
